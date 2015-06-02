cfa875e0a7f04fa58490392cbd8ffb21334bdd2b
Kukuh TW <kukuhtw@gmail.com>
2015-06-02 11:19:00 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,26 @@
-﻿<?php/*
-*/$mySQLserver = "localhost";
+<?php
+
+/*
+
+*/
+
+
+
+$mySQLserver = "localhost";
 $mySQLuser = "root";
 $mySQLpassword = "";
 $mySQLdefaultdb = "kronologger2015";
 $host = "localhost/kronologger2015";
-$link = mysql_connect($mySQLserver, $mySQLuser, $mySQLpassword) or die ("Could not connect to MySQL");mysql_select_db ($mySQLdefaultdb) or die ("Could not select database");
+
+$dsn = 'mysql:host=localhost;dbname=kronologger2015';
+$username = 'root';
+$password = '';
+
+
+$options = array(
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+);
+$link = new PDO($dsn, $username, $password, $options);
+
+
 ?>
