9fdcaa5d8bf91c1bf6dabe00112bbdf84f813b9b
Kukuh TW <kukuhtw@gmail.com>
2015-06-18 13:28:47 +0700

    add attribute pdo

--- db.php
+++ db.php
@@ -16,11 +16,12 @@ $dsn = 'mysql:host=localhost;dbname=kronologger2015';
 $username = 'root';
 $password = '';
 
-
 $options = array(
-    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
 );
 $link = new PDO($dsn, $username, $password, $options);
+$link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$link->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
 
 ?>
